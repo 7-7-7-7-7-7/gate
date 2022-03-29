34388fcca088a5d153feac37a937beda34747fda
Rodolfo Thiede <hello@rodolfothiede.me>
2022-03-28 22:08:29 -0300

    Edited the about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,24 +9,15 @@ const About = () => {
     return (
         <>
             <Header title={t('header.title')} description={t('header.description')} />
-            <div className={styles.aboutContainer}>
-                <div className={styles.gradient}></div>
-                <div className={styles.aboutBox}>
-                    <div className={styles.boxAbout}>
-                        <div className={styles.element}>
-                            <div className={styles.photo}></div>
-                        </div>
-                        My name is Rodolfo and I live in Santiago, Chile. I'm 26 years old and I'm a developer. I am mainly dedicated to the development of web platforms, in which I use the latest technologies currently in use. My mission as a professional is to create quality applications that are totally useful and elegant. I know that computing is mutating day by day and that's why I manage myself to be in constant learning.<br /><br />
-                        Since I was a child I had contact with the world of electronics and computing, having been born in an era where technology was in full growth. I learned to program at 16 years old out of curiosity and a couple of years later, I decided to study and make this my profession, so I entered in 2017 to study the career of Programmer analyst at INACAP, from which I graduated in 2019.
-                        Not everything in life is a profession, so I will tell you a little more about me: I like TV series, winter, photography, reading about science, technology, sociology, psychology... I enjoy music and its different genres. I don't exercise as much as I would like to, but I'm already working on that ;) For me animals are not food, nor are they clothes, nor ingredients of industrial products. I like to believe that little by little we are advancing as humanity towards a world free of cruelty to animals, but there is still a lot to do.
+            <div className={styles.aboutBox}>
+                <div className={styles.aboutText}>{t('about_me_one')}</div>
+                <div className={styles.aboutText}>
+                    <div className={styles.element}>
+                        <div className={styles.photo}></div>
                     </div>
+                    <div>{t('about_me_two')}</div>
                 </div>
-            </div>
-            <div className={styles.mobileAbout}>
-                <div className={styles.photo}></div>
-                <div className={styles.aboutMobileBox}>
-                    {t('lorem')}
-                </div>
+                <div className={styles.aboutText}>{t('about_me_three')}</div>
             </div>
         </>
     )
