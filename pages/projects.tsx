685678e31df9f23d441b5c083a127e9bfca0821e
Rodolfo Thiede <hello@rodolfothiede.me>
2022-04-18 21:46:51 -0400

    Finished the first part of the lab section

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -9,9 +9,60 @@ const Projects = () => {
     return (
         <>
             <Header title={t('header.title')} description={t('header.description')} />
-            <div className={styles.projectsBox}>
+            <div className={styles.projectsContainer}>
                 <div className={styles.projectBox}>
-
+                    <div className={styles.titlePage}>Proyectos</div>
+                    <div className={styles.projects}>
+                        <div className={styles.project}>
+                            <div className={styles.projectName}>Blog</div>
+                            <div className={styles.projectIcon}><i className="fa-solid fa-book"></i></div>
+                            <div className={styles.projectDescription}>
+                                Aplicación RESTful básica que gestiona artículos de un blog.
+                            </div>
+                            <div className={styles.projectTech}>
+                                <span>Ruby</span>
+                                <span>Rails 5</span>
+                            </div>
+                            <div className={styles.projectLink}>Repo <i className="fa-solid fa-up-right-from-square"></i></div>
+                        </div>
+                        <div className={styles.project}>
+                            <div className={styles.projectName}>Pokédex</div>
+                            <div className={styles.projectIcon}><i className="fa-solid fa-mobile-retro"></i></div>
+                            <div className={styles.projectDescription}>
+                                Aplicación web que consume una API que contiene los datos e imágenes de todos los Pokémon.
+                            </div>
+                            <div className={styles.projectTech}>
+                                <span>Angular</span>
+                                <span>REST</span>
+                            </div>
+                            <div className={styles.projectLink}>Repo <i className="fa-solid fa-up-right-from-square"></i></div>
+                        </div>
+                        <div className={styles.project}>
+                            <div className={styles.projectName}>Calculator.js</div>
+                            <div className={styles.projectIcon}><i className="fa-solid fa-calculator"></i></div>
+                            <div className={styles.projectDescription}>
+                                Calculadora que realiza operaciones básicas programada usando código base.
+                            </div>
+                            <div className={styles.projectTech}>
+                                <span>JavaScript</span>
+                                <span>CSS 3</span>
+                                <span>HTML 5</span>
+                            </div>
+                            <div className={styles.projectLink}>Repo <i className="fa-solid fa-up-right-from-square"></i></div>
+                        </div>
+                        <div className={styles.project}>
+                            <div className={styles.projectName}>Cookie importer</div>
+                            <div className={styles.projectIcon}><i className="fa-solid fa-cookie"></i></div>
+                            <div className={styles.projectDescription}>
+                                Extensión de navegadores modernos que permite la importación de cookies.
+                            </div>
+                            <div className={styles.projectTech}>
+                                <span>WebExtensions</span>
+                                <span>JavaScript</span>
+                            </div>
+                            <div className={styles.projectLink}>Repo <i className="fa-solid fa-up-right-from-square"></i></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
